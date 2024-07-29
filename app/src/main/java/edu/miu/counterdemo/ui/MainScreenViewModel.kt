31a3e1eea7caee9c9cfe58fad6fbd610c674d0df
Bright Gee Varghese <brightlsom@gmail.com>
2024-07-29 12:09:17 -0500

    Updated decrement with constraint.

--- app/src/main/java/edu/miu/counterdemo/ui/MainScreenViewModel.kt
+++ app/src/main/java/edu/miu/counterdemo/ui/MainScreenViewModel.kt
@@ -1,5 +1,6 @@
 package edu.miu.counterdemo.ui
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
@@ -22,15 +23,21 @@ class MainScreenViewModel: ViewModel() {
     var increment = {
         viewModelScope.launch {
             _counter.value.let {
-                _counter.value = it.copy(counter = it.counter.plus(1))
+                _counter.value = it.copy(counter = it.counter.plus(1), errorMessage = null)
             }
+            Log.d("MainScreenViewModel", "increment: ${_counter.value.counter}")
         }
     }
     var decrement = {
         viewModelScope.launch {
+            if (_counter.value.counter == 0) {
+                _counter.value = CounterUIState(errorMessage = "Counter cannot be less than 0")
+                return@launch
+            }
             _counter.value.let {
                 _counter.value = it.copy(counter = it.counter.minus(1))
             }
+            Log.d("MainScreenViewModel", "decrement: ${_counter.value.counter}")
         }
     }
 }
